287919257dee421a603019af26d5b451ef5ab884
Florent PEREZ <florent.perez@nokia.com>
2018-03-16 17:53:23 +0200

    foliezhgfhz

--- hello.cc
+++ hello.cc
@@ -3,6 +3,7 @@ using namespace std;
 
 int main() 
 {
-    cout << "Hello, World is for losers!";
+    cout << "Hello, World is for losers!" << endl;
+    cout << "git branches are strange!!"
     return 0;
 }

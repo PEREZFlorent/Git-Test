2a2c27fb878fa57177c9e01ea86ae6b911f7b97f
Florent PEREZ <florent.perez@nokia.com>
2018-03-16 18:04:58 +0200

    Hello.cc is now compilable and its executable is names hello

--- hello.cc
+++ hello.cc
@@ -4,6 +4,6 @@ using namespace std;
 int main() 
 {
     cout << "Hello, World is for losers!" << endl;
-    cout << "git branches are strange!!"
+    cout << "git branches are strange!!" << endl;
     return 0;
 }
